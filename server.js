f28e58a68e932e7ad1be246e3aa90c52722f6ace
jericam832 <jericam32@gmail.com>
2019-11-12 09:58:18 -0800

    added CSS

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
 
