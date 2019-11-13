ffff962efafc4fe07738dcc04c3716b973997007
jericam832 <jericam32@gmail.com>
2019-11-13 12:08:40 -0800

    pulling from local database

--- data/books.sql
+++ data/books.sql
@@ -13,5 +13,4 @@ CREATE TABLE books (
 
 INSERT INTO books
   (book_id, title, author, isbn, image_url, description, bookshelf)
-VALUES('2342435fwrw', 'sample book title', 'Some Guy', 532211455625, 'http:
-//placehold.it/200x300', 'my favorite book to read about a thing', 'mystery');
+VALUES('2342435fwrw', 'sample book title', 'Some Guy', 532211455625, 'http://placehold.it/200x300', 'my favorite book to read about a thing', 'mystery');
