f2a475d8319b62eb52763c0efe08373b84c3c3b1
jericam832 <jericam32@gmail.com>
2019-11-17 11:43:00 -0800

    Fix some issues

---
+++ public/app.js
@@ -0,0 +1,17 @@
+'use strict';
+// $('#new-book').show();
+// $('#book-template').show();
+//   $('#add-book').on( 'click', function () {
+//     $('#book-template').toggle();
+//     $('.new-book').toggle();
+// })
+
+$('.formfield').hide();
+$('button#edit-button').on('click', ()=>{
+  console.log(el);
+  $(this).siblings('section').toggle();
+  // $('#addition').toggle();
+  // $('.select-btn').hide();
+  // $('#edit-button').hide();
+})
+
